ce9f649b2dc67638ea95547d0ab773e25a6d9cef
parikhk <keyur.parikh@yahoo.com>
2021-12-10 10:11:29 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const submitConsent = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    const response = await axios({
+      method: "post",
+      url: "https://api.notifications.service.gov.uk/v2/notifications/email",
+      data: {
+        email_address: "keyur.parikh@digital.mod.uk",
+        template_id: "8e449ecc-c723-4d71-bde9-c91101646044",
+        personalisation: {
+          feedback: "Test",
+        },
+      },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI0NDJkNjQ2NC1mYzQ0LTRkODgtODY5MC1jM2Q1YjAzYTc2ZDkiLCJpYXQiOjE2MzkwODM3NzN9.5ddVBKxh38OXKdf8TahkT7bE7tafCw-3N_lCzVogzfw",
+      },
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -18,6 +38,10 @@ function App() {
         >
           Learn React
         </a>
+
+        <button type="submit" onClick={submitConsent}>
+          Submit
+        </button>
       </header>
     </div>
   );
